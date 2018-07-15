938a09140d831d4655b6c799492c246ec9fbf685
dlwfuture <dlw_future@hotmail.com>
2018-07-15 00:35:09 -0300

    feat: Changes to ListBooks and SearchBook use API instead of mocked data
    This commit changes all the mocked data to use BooksAPI instead.

--- src/App.js
+++ src/App.js
@@ -7,20 +7,25 @@ import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    bookList: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+        this.setState({
+          bookList: books
+        })
+    })
   }
 
   render() {
     return (
       <div className="app">
         <Route path='/search' component={SearchBook} />
-        <Route exact path='/' component={ListBooks} />
+        <Route exact path='/' render={() => (
+              <ListBooks bookList={this.state.bookList}></ListBooks>
+            )
+        } />
       </div>
     )
   }
--- src/SearchBook.js
+++ src/SearchBook.js
@@ -1,29 +1,64 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 
 class SearchBook extends Component {
-    render(){
+    state = {
+        booksList: []
+    }
+
+    searchBook = (e) => {
+        const searchText = e.target.value
+        searchText && searchText.length > 0 && BooksAPI.search(searchText).then(
+            res => {
+                const isArray = Array.isArray(res)
+                isArray && this.setState({ booksList: res }) 
+                !isArray && res && console.log(res.error)
+            }
+        )
+    }
+
+    render() {
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <Link   to='/'
-                            className='close-search'>
-                            Close
+                    <Link to='/'
+                        className='close-search'>
+                        Close
                     </Link>
                     <div className="search-books-input-wrapper">
-                        {/*
-                        NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                        You can find these search terms here:
-                        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                        you don't find a specific author or title. Every search is limited by search terms.
-                        */}
-                        <input type="text" placeholder="Search by title or author"/>
+                        <input type="text" onChange={this.searchBook} placeholder="Search by title or author" />
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                    <ol className="books-grid">
+                        {
+                            this.state.booksList && this.state.booksList.map(book => (
+                                <li key={book.id}>
+                                    <div className="book">
+                                        <div className="book-top">
+                                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+                                            <div className="book-shelf-changer">
+                                                <select>
+                                                    <option value="move" disabled>Move to...</option>
+                                                    <option value="currentlyReading">Currently Reading</option>
+                                                    <option value="wantToRead">Want to Read</option>
+                                                    <option value="read">Read</option>
+                                                    <option value="none">None</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div className="book-title">{book.title}</div>
+                                        {
+                                            book.authors && book.authors.map((author, authorIndex) => (
+                                                <div key={authorIndex} className="book-authors">{author}</div>
+                                            ))
+                                        }
+                                    </div>
+                                </li>
+                            ))
+                        }
+                    </ol>
                 </div>
             </div>
         )
